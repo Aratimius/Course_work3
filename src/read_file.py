6152f604780c177ae526a8038b5227dc67842944
Aratimius <aratimius@gmail.com>
2023-08-15 00:49:40 +0300

    add read_file.py and test_read_file.py

---
+++ src/read_file.py
@@ -0,0 +1,11 @@
+import json
+
+def get_all_operations():
+    '''Прочитает файл .json и вернет содержимое'''
+
+    with open('/media/artur/Artur files/PythonProjects/Course_work_3/operations.json', 'r') as file:
+        operations = file.read()
+        all_operations = json.loads(operations)
+
+        return all_operations
+
