59553091347bfb45690334e95127e7e39d78c8c6
Aratimius <aratimius@gmail.com>
2023-08-15 06:14:52 +0300

    create main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+"""
+обрабатываемый файл вызывается в read_file.py
+сортируется в get_sort.py
+данные извлекаются в get_EXECUTED.py
+данные обрабатываются в get_out.py
+Данные выводятся в терминал в get_operations.py
+
+Результат работы программы:
+"""
+
+from Course_work3.src.get_operations import get_five_last_operations
+
+
+get_five_last_operations()
