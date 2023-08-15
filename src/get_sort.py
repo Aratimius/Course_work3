307fcd6038c00401ae154610f8205cf2a8587cd6
Aratimius <aratimius@gmail.com>
2023-08-15 03:28:56 +0300

    fix get_sort.py, test_get_sort.py and operations.json

--- src/get_sort.py
+++ src/get_sort.py
@@ -1,12 +1,16 @@
 from Course_work3.src.read_file import get_all_operations
 
 
-def get_sorted():
+def get_sorted(data = get_all_operations()):
     ''' Сортировка данных по дате
     От самого позднего к самому раннему'''
 
-    data = get_all_operations()
-    sorted_data = sorted(data, key=lambda operation: operation['date'], reverse=True)
+    new_data = []
+    for operation in data:
+        if 'date' not in operation.keys():
+            continue
+        else: new_data.append(operation)
+
+    sorted_data = sorted(new_data, key=lambda operation: operation['date'], reverse=True)
 
     return sorted_data
-print(get_sorted())
