f2e182b59b5274131981372c42b3b4d72d69e361
Aratimius <aratimius@gmail.com>
2023-08-15 05:25:29 +0300

    fix read_file.py

--- src/read_file.py
+++ src/read_file.py
@@ -3,7 +3,7 @@ import json
 def get_all_operations():
     '''Прочитает файл .json и вернет содержимое'''
 
-    with open('/media/artur/Artur files/PythonProjects/Course_work_3/operations.json', 'r') as file:
+    with open('/media/artur/Artur_files/PythonProjects/Course_work_3/operations.json', 'r') as file:
         operations = file.read()
         all_operations = json.loads(operations)
 
